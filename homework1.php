428b8c2e98c04ac841bae6b866c381d74087b3e5
krishnagupta71 <gupta.krishna71@gmail.com>
2021-09-24 17:24:13 -0700

    homework 1

---
+++ homework1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Homework - 1</h1>
+<p>Setup you own web programming environment</p>
+<h3>Krishna Gupta - 015721199</h3>
+<?php
+echo "My first PHP script!";
+?> 
+<div></div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>CMPE - 272</h1>
+<h2>Prof Richard Sinn</h2>
+<h3>Krishna Gupta - 015721199</h3>
+
+<a href="/homework1.php"> HomeWork 1 </a><br>
+<?php
+echo "My first PHP script!";
+?> 
+<div></div>
+
+</body>
+</html>
