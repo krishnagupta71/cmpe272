3618524579b06676dc1b5b452525d5589c11246a
Krishna Gupta <gupta.krishna71@gmail.com>
2021-09-24 17:30:01 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <h2>Prof Richard Sinn</h2>
 <h3>Krishna Gupta - 015721199</h3>
 
-<a href="/homework1.php"> HomeWork 1 </a><br>
+<a href="homework1.php"> HomeWork 1 </a><br>
 <?php
 echo "My first PHP script!";
 ?> 
